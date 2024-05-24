fe1a29cd72709c777bb3d65fa5e730caf72e0b05
gurjantsingh3 <tr.gurjant@gmail.com>
2024-05-24 18:23:42 +0530

    gf

---
+++ harman/src/app/page.tsx
@@ -0,0 +1,11 @@
+import { ContactForm } from "@/components/contact form";
+import { Main } from "@/components/main";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <Main />
+    </>
+  );
+}
